b5b35ec236d3a7c73a84098fe476e62b2eee1e87
hamzona <hamzadjedovic2000@gmail.com>
2023-08-09 15:11:02 +0200

    fix

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
 import Login from "./features/auth/Login";
 import DashBoard from "./components/DashBoard";
 import RequireAuth from "./components/RequireAuth";
@@ -8,17 +8,19 @@ import Singup from "./features/auth/Singup";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Public />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/singup" element={<Singup />} />
-      <Route element={<PresistLogin />}>
-        <Route element={<RequireAuth />}>
-          <Route path={"/dashboard/:id"} element={<DashBoard />} />
-          <Route path={"/dashboard"} element={<DashBoard />} />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Public />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/singup" element={<Singup />} />
+        <Route element={<PresistLogin />}>
+          <Route element={<RequireAuth />}>
+            <Route path={"/dashboard/:id"} element={<DashBoard />} />
+            <Route path={"/dashboard"} element={<DashBoard />} />
+          </Route>
         </Route>
-      </Route>
-    </Routes>
+      </Routes>
+    </BrowserRouter>
   );
 }
 export default App;
