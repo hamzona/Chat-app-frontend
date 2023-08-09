20e7637b3b9ed9cc2ed03049170825c22f513caa
hamzona <hamzadjedovic2000@gmail.com>
2023-08-09 16:32:18 +0200

    fix

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ import Login from "./features/auth/Login";
 import DashBoard from "./components/DashBoard";
 import RequireAuth from "./components/RequireAuth";
 import PresistLogin from "./components/PresistLogin";
-import Public from "./components/Public";
+//import Public from "./components/Public";
 import Singup from "./features/auth/Singup";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Public />} />
-      <Route path="/login" element={<Login />} />
+      {/* <Route path="/" element={<Public />} /> */}
+      <Route path="/" element={<Login />} />
       <Route path="/singup" element={<Singup />} />
       <Route element={<PresistLogin />}>
         <Route element={<RequireAuth />}>
